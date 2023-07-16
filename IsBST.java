73f46d64fd461b67c4c99c42ed7e2172386b77cf
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2023-07-16 10:17:55 +0530

    Create IsBST.java

---
+++ IsBST.java
@@ -0,0 +1,48 @@
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+    
+    // Helper function to check if a binary tree is a BST
+    private boolean isBSTUtil(Node node, int left, int right) {
+        // Base case: an empty tree is considered a BST
+        if (node == null)
+            return true;
+        
+        // If the current node violates the BST property, return false
+        if (node.data <= left || node.data >= right)
+            return false;
+        
+        // Recursively check the left and right subtrees
+        return isBSTUtil(node.left, left, node.data) && isBSTUtil(node.right, node.data, right);
+    }
+    
+    // Function to check if a binary tree is a BST
+    public boolean isBST() {
+        return isBSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Construct the binary tree
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(2);
+        tree.root.left = new Node(1);
+        tree.root.right = new Node(3);
+        
+        // Check if the tree is a BST
+        if (tree.isBST())
+            System.out.println("The tree is a BST.");
+        else
+            System.out.println("The tree is not a BST.");
+    }
+}
