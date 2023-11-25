9edbe8dcc06b765dd4cace0b4f725f95b874c941
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2023-11-25 19:45:41 +0530

    Update ShuffleInteger.java

--- ShuffleInteger.java
+++ ShuffleInteger.java
@@ -1,1 +1,16 @@
 
+class  Solution
+{
+    void shuffleArray(long arr[], int n)
+    {
+        // Your code goes here
+        long[] temp=new long[n];
+        int index = 0;
+        for(int i=0;i<n/2;i++){
+            temp[index++]=arr[i];
+            temp[index++]=arr[i+n/2];
+        }
+        for(int i=0;i<n;i++)
+            arr[i]=temp[i];
+        }
+}
