0dfaf16f7fa923687eec9931cd156df69cef9681
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-04-22 10:38:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximize Number of 1's/maximize-number-of-1s.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            int m = Integer.parseInt(br.readLine().trim());
+
+            int ans = new Solve().findZeroes(arr, n, m);
+
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    // m is maximum of number zeroes allowed to flip
+    int findZeroes(int arr[], int n, int m) {
+        int res =0;
+        int countZero = 0, left = 0, right =0;
+        while(right <n){
+            if(arr[right] == 0)countZero++;
+            while(countZero > m){
+                if(arr[left] == 0)countZero--;
+                left++;
+            }
+            res = Math.max(res, right - left +1);
+            right++;
+        }
+        return res;
+    }
+}
