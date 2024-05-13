61b470ccb15be603e066ee7c2a57fca87f77fad3
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-05-13 15:48:53 +0530

    Added Solution - GfG to GitHub

---
+++ School/Display longest name/display-longest-name.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class StringArray
+{
+    public static String[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        return s;
+    }
+
+    public static void print(String[] a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<String> a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            String[] names = StringArray.input(br, n);
+            
+            Solution obj = new Solution();
+            String res = obj.longest(n, names);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static String longest(int n, String[] names) {
+        // code here
+        int max =0;
+        String s = "";
+        for(String  i: names){
+            if(i.length() > max){
+                max = i.length();
+                s =i;
+            }
+        }
+        return s;
+    }
+}
+        
