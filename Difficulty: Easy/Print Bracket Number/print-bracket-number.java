a1b9cb6c09166b8dada1e27eac3560cdca1efae6
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-06-23 11:00:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Bracket Number/print-bracket-number.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+            ArrayList<Integer> result = ob.bracketNumbers(S);
+            for (int value : result) out.print(value + " ");
+            out.println();
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    ArrayList<Integer> bracketNumbers(String str) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        int count=1;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) =='('){
+                ans.add(count);
+                st.push(count);
+                count++;
+            }else if(str.charAt(i) == ')'){
+                ans.add(st.pop());
+            }
+        }
+        
+        return ans;
+    }
+};
