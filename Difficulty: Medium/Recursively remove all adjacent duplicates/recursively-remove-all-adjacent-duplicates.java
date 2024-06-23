40f415c8c11be40c60713c66561a975f8799e8b7
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-06-23 15:13:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Recursively remove all adjacent duplicates/recursively-remove-all-adjacent-duplicates.java
+++ Difficulty: Medium/Recursively remove all adjacent duplicates/recursively-remove-all-adjacent-duplicates.java
@@ -23,30 +23,29 @@ class GFG {
 
 class Solution{
     String rremove(String s) {
-        if(s.isEmpty())return s;
-        StringBuilder ans = new StringBuilder();
-        helper(ans, s);
-        return ans.toString();
-    }
-    void helper(StringBuilder sb, String s){
-        boolean[] adjacentFlag = new boolean[s.length()];
-        //Tag adjacent as true.
+        // code here
+        String s1 = " ";
+        while(s.length() != s1.length()){
+            s1 =s;
+            s= rem(s);
+        }
+        return s;
         
-        for(int i=1; i<s.length(); i++){
-            if(s.charAt(i-1) == s.charAt(i)){
-                adjacentFlag[i] =adjacentFlag[i-1] = true;
+    }
+    String rem(String s){
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        int i=0;
+        while(i<n){
+            if(i<n-1 && s.charAt(i) == s.charAt(i+1)){
+                while(i<n-1 &&s.charAt(i) == s.charAt(i+1)){
+                    i++;
+                }
+            }else{
+                sb.append(s.charAt(i));
             }
+            i++;
         }
-        StringBuilder rem = new StringBuilder();
-        //Find remaing elements not tagged as true.
-        for(int i=0; i<adjacentFlag.length; i++){
-            if(!adjacentFlag[i])rem.append(s.charAt(i));
-        }
-        // 0 Ajacent element(s) exist, return!.
-        if(rem.length() == s.length()){
-            sb.append(s);
-            return;
-        }
-        helper(sb, rem.toString());
+        return sb.toString();
     }
 }
