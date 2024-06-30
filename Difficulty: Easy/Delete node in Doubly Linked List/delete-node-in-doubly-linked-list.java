f694c8cc90d2e01ac0a089d88e1ecb14729c340a
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-06-30 12:32:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete node in Doubly Linked List/delete-node-in-doubly-linked-list.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        prev = null;
+    }
+
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            Node head = null;
+            Node tail = head;
+
+            for (int i = 0; i < n; i++) {
+                int temp = sc.nextInt();
+                if (head == null) {
+                    head = new Node(temp);
+                    tail = head;
+                } else {
+                    Node newNode = new Node(temp);
+                    tail.next = newNode;
+                    newNode.prev = tail;
+                    tail = newNode;
+                }
+            }
+            int x = sc.nextInt();
+
+            Solution obj = new Solution();
+            Node res = obj.deleteNode(head, x);
+
+            Node.printList(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for doubly Link List Node
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int x){
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    public Node deleteNode(Node head, int x) {
+        // If the node to be deleted is the head node
+        if (x == 1) {
+            if (head != null) {
+                head = head.next;
+                if (head != null) {
+                    head.prev = null;
+                }
+            }
+            return head;
+        }
+        
+        Node temp = head;
+
+        // Traverse to the node to be deleted
+        for (int i = 1; i < x && temp != null; i++) {
+            temp = temp.next;
+        }
+
+        // If the node to be deleted is found
+        if (temp != null) {
+            if (temp.prev != null) {
+                temp.prev.next = temp.next;
+            }
+            if (temp.next != null) {
+                temp.next.prev = temp.prev;
+            }
+        }
+
+        return head;
+    }
+}
