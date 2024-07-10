2a2a6765de7077c13327b6d2bbd66eff032ed4c9
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-07-10 23:40:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest square formed in a matrix/largest-square-formed-in-a-matrix.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int mat[][] = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    mat[i][j] = sc.nextInt();
+                }
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.maxSquare(n, m, mat));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int maxSquare(int n, int m, int matrix[][]) {
+        // code here
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        // int n = matrix.length;
+        // int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        int maxSideLength = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    }
+                    maxSideLength = Math.max(maxSideLength, dp[i][j]);
+                }
+            }
+        }
+
+        return maxSideLength;
+    }
+}
