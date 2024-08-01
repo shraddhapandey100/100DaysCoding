d49acac0c9aed5bf026690325df7c99810b35b63
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-08-01 16:23:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
@@ -1,29 +1,24 @@
 //{ Driver Code Starts
 import java.io.*;
 import java.util.*;
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+
+class GFG {
+    public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        
-        while(t-- > 0)
-        {
+
+        while (t-- > 0) {
             int r = sc.nextInt();
             int c = sc.nextInt();
-            
+
             int matrix[][] = new int[r][c];
-            
-            for(int i = 0; i < r; i++)
-            {
-                for(int j = 0; j < c; j++)
-                 matrix[i][j] = sc.nextInt();
+
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c; j++) matrix[i][j] = sc.nextInt();
             }
             Solution ob = new Solution();
-            ArrayList<Integer> ans = ob.spirallyTraverse(matrix, r, c);
-            for (Integer val: ans) 
-                System.out.print(val+" "); 
+            ArrayList<Integer> ans = ob.spirallyTraverse(matrix);
+            for (Integer val : ans) System.out.print(val + " ");
             System.out.println();
         }
     }
@@ -31,30 +26,43 @@ class GFG
 // } Driver Code Ends
 
 
-class Solution
-{
-    //Function to return a list of integers denoting spiral traversal of matrix.
-    static ArrayList<Integer> spirallyTraverse(int matrix[][], int r, int c)
-    {
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        int top =0, left =0, right = c-1, bottom = r-1;
-        while(top<= bottom && left <=right){
-            for(int i=left; i<=right; i++){
+class Solution {
+    // Function to return a list of integers denoting spiral traversal of matrix.
+    public ArrayList<Integer> spirallyTraverse(int matrix[][]) {
+        // code here
+        int r = matrix.length;
+        int c = matrix[0].length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(c ==1){
+            for(int i=0; i<r; i++){
+                ans.add(matrix[i][0]);
+            }
+            return ans;
+        }
+        if(r ==1){
+            for(int i=0; i<c; i++){
+                ans.add(matrix[0][i]);
+            }
+            return ans;
+        }
+        int top =0, right = c-1, left =0, bottom = r-1;
+        while((top <= bottom) && (left <= right)){
+            for(int i= left; i<=right; i++){
                 ans.add(matrix[top][i]);
             }
             top++;
-            for(int i=top; i<=bottom; i++){
+            for(int i= top; i<=bottom; i++){
                 ans.add(matrix[i][right]);
             }
             right--;
-            if(top<=bottom){
-                for(int i=right; i>=left; i--){
+            if(top <= bottom){
+                for(int i = right; i>= left ; i--){
                     ans.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
-            if(left <=right){
-                for(int i=bottom; i>=top; i--){
+            if(left <= right){
+                for(int i= bottom; i>=top; i--){
                     ans.add(matrix[i][left]);
                 }
                 left++;
