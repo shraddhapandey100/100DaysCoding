79dd7f1070f88a838232b4ab9ce4a40f26446b12
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-09-23 14:45:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.java
@@ -7,8 +7,7 @@ import java.util.*;
 class GFG {
     // Driver code
     public static void main(String[] args) throws Exception {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine().trim());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine().trim());
@@ -19,7 +18,7 @@ class GFG {
                 a[i] = Integer.parseInt(str[i]);
             }
 
-            int[] ans = new Solve().findTwoElement(a, n);
+            int[] ans = new Solve().findTwoElement(a);
             System.out.println(ans[0] + " " + ans[1]);
         }
     }
@@ -30,27 +29,24 @@ class GFG {
 // User function Template for Java
 
 class Solve {
-    int[] findTwoElement(int arr[], int n) {
-        int res[] = new int[2];
-        int repeating =-1;
-        int missing = -1;
-        int hash[] = new int[n+1];
+    int[] findTwoElement(int a[]) {
+        int n = a.length;
+        int[] hash = new int[n+1];
+        //Update the hash array.
         for(int i=0; i<n; i++){
-            hash[arr[i]]++;
+            hash[a[i]]++;
         }
-        for(int j=1; j<n+1; j++){
-            if(hash[j] == 2){
-                repeating = j;
-            }
-            if(hash[j] == 0){
-                missing = j;
-            }
+        //Find the repeating and missing number.
+        int repeating = -1, missing = -1;
+        for(int i=1; i<=n; i++){
+            if(hash[i] == 2)repeating = i;
+            else if(hash[i] ==0)missing = i;
             if(repeating != -1 && missing != -1){
                 break;
             }
         }
-        res[0] = repeating;
-        res[1] = missing;
-        return res;
+        int[] ans = {repeating , missing};
+        return ans;
+        
     }
 }
