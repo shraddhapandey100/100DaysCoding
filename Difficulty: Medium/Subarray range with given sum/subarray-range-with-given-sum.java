a84b54393cf62a4b1f35c39ed75220aa8278ec07
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-10-15 06:58:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray range with given sum/subarray-range-with-given-sum.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to count the number of subarrays which adds to the given sum.
+    static int subArraySum(int arr[], int tar) {
+        // add your code here
+        int count =0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int cur_sum =0;
+        for(int i=0; i<arr.length; i++){
+            cur_sum +=arr[i];
+            count+=map.getOrDefault(cur_sum- tar , 0);
+            map.put(cur_sum, map.getOrDefault(cur_sum , 0)+1);
+        }
+        return count;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int tar = Integer.parseInt(br.readLine());
+            Solution obj = new Solution();
+            int res = obj.subArraySum(arr, tar);
+
+            System.out.println(res);
+            // System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
