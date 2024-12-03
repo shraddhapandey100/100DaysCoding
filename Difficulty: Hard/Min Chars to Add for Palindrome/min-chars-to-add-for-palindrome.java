e04abaabd500e5f450dfa08f30d0c33730960b6e
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-12-03 19:03:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Min Chars to Add for Palindrome/min-chars-to-add-for-palindrome.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public static int minChar(String s) {
+        // Write your code here
+        int N = s.length();
+        String reversedStr = new StringBuilder(s).reverse().toString();
+        String modifiedStr = s +"$"+reversedStr;
+        int[] lps = new int[2*N +1];
+        computeLPSArray(modifiedStr, lps);
+        return N - lps[2*N];
+    }
+    private static void computeLPSArray(String str, int[]lps){
+        int N = str.length();
+        int len =0;
+        int i=1;
+        while(i< N){
+            if(str.charAt(i) == str.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if(len != 0){
+                    len = lps[len -1];
+                }else{
+                    lps[i] =0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        var sc = new FastReader();
+        int test = sc.nextInt();
+        while (test-- > 0) {
+            String s = sc.nextString();
+            System.out.println(Solution.minChar(s));
+
+            System.out.println("~");
+        }
+    }
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            st = new StringTokenizer("");
+        }
+
+        private void read() throws IOException {
+            st = new StringTokenizer(br.readLine());
+        }
+
+        public String nextString() throws IOException {
+            while (!st.hasMoreTokens()) read();
+            return st.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(nextString());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(nextString());
+        }
+    }
+}
+// } Driver Code Ends
