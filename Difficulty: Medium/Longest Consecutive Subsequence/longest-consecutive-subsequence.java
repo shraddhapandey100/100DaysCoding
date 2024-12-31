70fbe763bc8ce27b0d7abb05534aa988fa452919
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2024-12-31 14:02:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -30,17 +30,37 @@ class Solution {
     // Function to return length of longest subsequence of consecutive integers.
     public int longestConsecutive(int[] arr) {
         // code here
-        Arrays.sort(arr);
-        int res =1, cnt =1;
-        for(int i=1; i<arr.length; i++){
-            if(arr[i] == arr[i-1]){
-                continue;
-            }else if(arr[i] == arr[i-1] +1){
-                cnt++;
-            }else{
-                cnt =1;
+        // Arrays.sort(arr);
+        // int res =1, cnt =1;
+        // for(int i=1; i<arr.length; i++){
+        //     if(arr[i] == arr[i-1]){
+        //         continue;
+        //     }else if(arr[i] == arr[i-1] +1){
+        //         cnt++;
+        //     }else{
+        //         cnt =1;
+        //     }
+        //     res = Math.max(res, cnt);
+        // }
+        // return res;
+        Set<Integer> st = new HashSet<>();
+        int res =0;
+        //Hash all the array elements.
+        for(int val: arr){
+            st.add(val);
+        }
+        //Check each possible sequence from the start then update optional length.
+        for(int val : arr){
+            if(st.contains(val) && !st.contains(val -1)){
+                //Then check for next elements in the sequence.
+                int cur =val, cnt =0;
+                while(st.contains(cur)){
+                    st.remove(cur);
+                    cur++;
+                    cnt++;
+                }
+                res = Math.max(res, cnt);
             }
-            res = Math.max(res, cnt);
         }
         return res;
     }
