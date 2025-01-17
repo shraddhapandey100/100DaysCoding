1d3a43fce15fbefa64d1b63756fc485b20265237
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2025-01-17 13:16:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.java
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -6,22 +6,24 @@ import java.util.*;
 
 class GFG {
     public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         while (t > 0) {
-            int n = sc.nextInt();
-            int[] array = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
             for (int i = 0; i < n; i++) {
-                array[i] = sc.nextInt();
+                arr[i] = Integer.parseInt(inputLine[i]);
             }
             Solution ob = new Solution();
-            long[] ans = new long[n];
-            ans = ob.productExceptSelf(array);
+            int[] ans = new int[n];
+            ans = ob.productExceptSelf(arr);
 
             for (int i = 0; i < n; i++) {
                 System.out.print(ans[i] + " ");
             }
             System.out.println();
+            System.out.println("~");
             t--;
         }
     }
@@ -31,22 +33,26 @@ class GFG {
 
 
 // User function Template for Java
-
 class Solution {
-    public static long[] productExceptSelf(int nums[]) {
-        int k =0;
-        long sum =1;
-        long ans[] =new long[nums.length];
-        for(int i=0; i<nums.length; i++){
-            for(int j=0; j<nums.length; j++){
-                if(i != j){
-                    sum *=nums[j];
-                }
-            }
-            ans[k++] = sum;
-            sum =1;
+    public static int[] productExceptSelf(int arr[]) {
+        // code here
+        int n = arr.length;
+        int[]prefProduct = new int[n];
+        int[] suffProduct = new int[n];
+        int[]res = new int[n];
+        prefProduct[0] =1;
+        //Construct the prefProduct array.
+        for(int i=1; i<n; i++){
+            prefProduct[i] = arr[i-1]*prefProduct[i-1];
         }
-        return ans;
+        //Construct the suffProduct array.
+        suffProduct[n-1] = 1;
+        for(int j = n-2; j>=0; j--){
+            suffProduct[j] = arr[j+1]*suffProduct[j+1];
+        }
+        for(int i=0; i<n; i++){
+            res[i] = prefProduct[i]*suffProduct[i];
+        }
+        return res;
     }
 }
-    
