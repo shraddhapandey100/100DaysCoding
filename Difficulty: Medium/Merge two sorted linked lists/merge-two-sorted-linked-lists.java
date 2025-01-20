475560349101350754caf273f529809df6357967
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2025-01-20 23:28:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
@@ -1,73 +1,73 @@
 //{ Driver Code Starts
 import java.util.*;
 
-class Node
-{
+class Node {
     int data;
     Node next;
-    Node(int d) {
-        data = d; 
+
+    Node(int x) {
+        data = x;
         next = null;
     }
 }
 
-
-class MergeLists
-{
-    Node head;
-
-
-
-  /* Function to print linked list */
-   public static void printList(Node head)
-    {
-        
-        while (head!= null)
-        {
-           System.out.print(head.data+" ");
-           head = head.next;
-        }  
+public class Main {
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
         System.out.println();
+        System.out.println("~");
     }
-	
-	 
- 
-     /* Driver program to test above functions */
-    public static void main(String args[])
-    {
-       
-         
-        /* Constructed Linked List is 1->2->3->4->5->6->
-           7->8->8->9->null */
-         Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 
-		 while(t>0)
-         {
-			int n1 = sc.nextInt();
-			int n2 = sc.nextInt();
-			Node head1 = new Node(sc.nextInt());
-            Node tail1 = head1;
-            for(int i=0; i<n1-1; i++)
-            {
-                tail1.next = new Node(sc.nextInt());
-                tail1 = tail1.next;
+
+    // Insert node into the list in a sorted manner
+    public static Node insertSorted(Node head, int data) {
+        Node newNode = new Node(data);
+        if (head == null || head.data >= data) {
+            newNode.next = head;
+            return newNode;
+        }
+
+        Node current = head;
+        while (current.next != null && current.next.data < data) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+        while (T-- > 0) {
+            Node head1 = null;
+            Node head2 = null;
+
+            // Reading first linked list input
+            String input1 = sc.nextLine();
+            String[] elems1 = input1.split(" ");
+            for (String elem : elems1) {
+                head1 = insertSorted(head1, Integer.parseInt(elem));
             }
-			Node head2 = new Node(sc.nextInt());
-            Node tail2 = head2;
-            for(int i=0; i<n2-1; i++)
-            {
-                tail2.next = new Node(sc.nextInt());
-                tail2 = tail2.next;
+
+            // Reading second linked list input
+            String input2 = sc.nextLine();
+            String[] elems2 = input2.split(" ");
+            for (String elem : elems2) {
+                head2 = insertSorted(head2, Integer.parseInt(elem));
             }
-			
-			LinkedList obj = new LinkedList();
-			Node head = obj.sortedMerge(head1,head2);
-			printList(head);
-			
-			t--;
-			
-         }
+
+            // Merging sorted linked lists
+            Solution obj = new Solution();
+            Node mergedHead = obj.sortedMerge(head1, head2);
+            printList(mergedHead); // Print the merged sorted list
+        }
+        sc.close();
     }
 }
 
@@ -75,9 +75,7 @@ class MergeLists
 
 
 /*
-  Merge two linked lists 
-  head pointer input could be NULL as well for empty list
-  Node is defined as 
+  Node is defined as
     class Node
     {
         int data;
@@ -86,32 +84,26 @@ class MergeLists
     }
 */
 
-class LinkedList
-{
-    //Function to merge two sorted linked list.
-    Node sortedMerge(Node head1, Node head2) {
-        Node ans = new Node(-1), curr = ans;
-        while(head1 != null && head2 != null){
-            if(head1.data < head2.data){
-                curr.next = new Node(head1.data);
-                curr = curr.next;
-                head1 = head1.next;
-            }else{
-                curr.next = new Node(head2.data);
-                curr = curr.next;
-                head2 = head2.next;
+class Solution {
+    Node sortedMerge(Node h1, Node h2) {
+        // code here
+        if(h1!=null ||h2!=null){
+            if(h1==null && h2 != null){
+                return h2;
+            }
+            else if(h1!=null && h2 == null){
+                return h1;
+            }
+            else{
+                if(h1.data>=h2.data){
+                    h2.next = sortedMerge(h1, h2.next);
+                    return h2;
+                }else{
+                    h1.next = sortedMerge(h1.next, h2);
+                    return h1;
+                }
             }
         }
-        while(head1 != null){
-            curr.next = new Node(head1.data);
-            curr = curr.next;
-            head1 = head1.next;
-        }
-        while(head2 != null){
-            curr.next=new Node(head2.data);
-            curr = curr.next;
-            head2 = head2.next;
-        }
-        return ans.next;
-   } 
+        return h1;
+    }
 }
