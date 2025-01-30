831130c475104612f1fedd30d773398807007e9e
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2025-01-30 21:38:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.java
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.java
@@ -17,6 +17,16 @@ class GFG {
             if (ans.size() == 0)
                 System.out.println("-1");
             else {
+                ans.sort((list1, list2) -> {
+                    int size = Math.min(list1.size(), list2.size());
+                    for (int i = 0; i < size; i++) {
+                        if (!list1.get(i).equals(list2.get(i))) {
+                            return list1.get(i) - list2.get(i);
+                        }
+                    }
+                    return list1.size() - list2.size();
+                });
+
                 for (int i = 0; i < ans.size(); i++) {
                     System.out.print("[");
                     for (int j = 0; j < ans.get(i).size(); j++)
@@ -25,6 +35,8 @@ class GFG {
                 }
                 System.out.println();
             }
+
+            System.out.println("~");
         }
     }
 }
@@ -32,78 +44,65 @@ class GFG {
 
 
 // User function Template for Java
-/*
 
-
-*/
-
-class Solution {
-    ArrayList<ArrayList<Integer>> ans;
-    public ArrayList<ArrayList<Integer>> nQueen(int n) {
-        ans = new ArrayList<>();
-        int[][] board = new int[n][n];
-        solve(0, board, n);
-        Collections.sort(ans, (a, b) -> {
-            for (int i = 0; i < a.size(); i++) {
-                if (!a.get(i).equals(b.get(i))) {
-                    return a.get(i) - b.get(i);
-                }
-            }
-            return 0;
-        });
-        return ans;
-    }
-    
-    public void solve(int row, int[][] board, int n)
-    {
-        if(row == n)
-        {
-            makeBoard(board, n);
-            return;
-        }
-        
-        for(int col=0; col<n; col++)
-        {
-            if(isSafe(row, col, board))
-            {
-                board[row][col] = 1;
-                solve(row+1, board, n);
-                board[row][col] = 0;
-            }
-        }
-    }
-    
-    public boolean isSafe(int row, int col, int[][] board)
-    {
-        // checking row upwards
-        for(int i=0; i<row; i++)
-        if(board[i][col] == 1)
-        return false;
-        
-        // checking left diagonal upwards
-        int maxLeft = Math.min(row, col);
-        for(int i=0; i<=maxLeft; i++)
-        if(board[row-i][col-i] == 1)
-        return false;
-        
-        // checking right diagonal upwards
-        int maxRight = Math.min(row, board.length-1-col);
-        for(int i=0; i<=maxRight; i++)
-        if(board[row-i][col+i] == 1)
-        return false;
-        
-        return true;
-    }
-    
-    public void makeBoard(int[][] board, int n)
-    {
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-            if(board[j][i] == 1)
-            arr.add(j+1);
-        }
-        ans.add(arr);
-    }
+class Solution
+{
+   static ArrayList<ArrayList<Integer>> nQueen(int n) 
+   {
+       // code here
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        int[][] arr = new int[n][n];
+        nQueens(arr,0,result,n);  
+        return result;
+   }
+   static void nQueens(int[][] arr,int row,ArrayList<ArrayList<Integer>> result,int n)
+   {
+       if(row==arr.length)
+       {
+           //collect answers and reinitialize a to remove the current answer 
+           ArrayList<Integer> answer = new ArrayList<>();
+           for(int i=0;i<arr.length;i++)
+           {
+               for(int j=0;j<arr.length;j++)
+               {
+                   if(arr[i][j]==1)
+                   answer.add(j+1);
+               }
+           }
+           result.add(answer);
+          arr = new int[n][n];
+          return;
+       }
+       
+       for(int col=0;col<n;col++)
+       {
+           if(isSafe(arr,row,col))
+           {
+               arr[row][col] = 1;
+               nQueens(arr,row+1,result,n);
+               arr[row][col] = 0;
+           }
+       }
+   }
+   public static boolean isSafe(int[][] arr,int row,int col)
+   {
+       //check up 
+       for(int i = row-1 ; i>=0 ; i--)
+         {
+             if(arr[i][col]==1)
+             return false;
+         }
+        //check diagonals
+        for(int i=row-1,j=col-1;i>=0 && j>=0 ; j--,i--)
+       {
+             if(arr[i][j]==1)
+             return false;
+       }
+         for(int i=row-1,j=col+1;i>=0 && j<arr[0].length ; j++,i--)
+       {
+             if(arr[i][j]==1)
+             return false;
+       }
+       return true;
+   }
 }
